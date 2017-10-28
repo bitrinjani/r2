d0e5ea974fa1314992b8ca5b14c0738241023bfc
user1 <user1>
2017-10-28 20:17:23 +0900

    handle SIGINT in Linux

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import AppRoot from './AppRoot';
+import { exec } from 'child_process';
 
+process.title = 'r2app';
 const app = new AppRoot();
 app.start();
 
+function exit() {  
+  exec(`pkill ${process.title}`);
+}
+
 process.on('SIGINT', () => {
+  console.log('SIGINT received. Stopping...');
   app.stop();
+  exit();
 });
 
 process.on('unhandledRejection', (reason, p) => {
   console.error(reason);
   app.stop();
+  exit();
 });
