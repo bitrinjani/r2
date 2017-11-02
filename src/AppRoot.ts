7d9db467a6eb9bbff4a5e03071f28e3b87300ea3
bitrinjani <bit.rinjani@gmail.com>
2017-11-02 17:25:47 +0900

    refactor AppRoot for unit test

--- src/AppRoot.ts
+++ src/AppRoot.ts
@@ -4,15 +4,18 @@ import 'reflect-metadata';
 import container from './container';
 import symbols from './symbols';
 import { Arbitrager } from './type';
+import { Container } from 'inversify';
 
 export default class AppRoot {
   private log = getLogger('AppRoot');
   private arbitrager: Arbitrager;
 
+  constructor(readonly ioc: Container = container) { }
+
   async start(): Promise<void> {
     try {
       this.log.info(intl.t('StartingTheService'));
-      this.arbitrager = container.get<Arbitrager>(symbols.Arbitrager);
+      this.arbitrager = this.ioc.get<Arbitrager>(symbols.Arbitrager);
       await this.arbitrager.start();
       this.log.info(intl.t('SuccessfullyStartedTheService'));
     } catch (ex) {
