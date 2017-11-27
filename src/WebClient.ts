ab544c1dfb3c86f25f809c55132faf671bc1d140
bitrinjani <bit.rinjani@gmail.com>
2017-11-27 18:46:25 +0900

    add timeout to HTTP request

--- src/WebClient.ts
+++ src/WebClient.ts
@@ -3,24 +3,28 @@ import fetch, { RequestInit as FetchRequestInit } from 'node-fetch';
 import { getLogger } from './logger';
 
 export default class WebClient {
+  static fetchTimeout = 5000;
   private log = getLogger('WebClient');
 
   constructor(public baseUrl: string) {
     this.baseUrl = baseUrl;
   }
 
-  async fetch<T>(path: string, init: FetchRequestInit = {}, verbose: boolean = true): Promise<T> {
+  async fetch<T>(
+    path: string,
+    init: FetchRequestInit = { timeout: WebClient.fetchTimeout },
+    verbose: boolean = true): Promise<T> {
     const url = this.baseUrl + path;
     this.log.debug(`Sending HTTP request... URL: ${url} Request: ${JSON.stringify(init)}`);
     const res = await fetch(url, init);
     let logText = `Response from ${res.url}. ` +
       `Status Code: ${res.status} (${res.statusText}) `;
-    this.log.debug(logText);    
+    this.log.debug(logText);
     const content = await res.text();
-    if (!res.ok) {      
+    if (!res.ok) {
       logText += `Content: ${content}`;
       throw new Error(`HTTP request failed. ${logText}`);
-    }    
+    }
     if (!content) {
       return {} as T;
     }
