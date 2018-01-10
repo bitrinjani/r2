850dd7cc1f562baf96a3deee7a6d5abaed79b297
bitrinjani <bit.rinjani@gmail.com>
2018-01-10 20:33:25 +0900

    update fatal error list

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,9 @@
 export const LOT_MIN_DECIMAL_PLACE = 3;
-export const fatalErrors = ['insufficient balance', 'Insufficient funds', 'Too Many Requests', 'Service Unavailable'];
+export const fatalErrors = [
+  'insufficient balance',
+  'Insufficient funds',
+  'Too Many Requests',
+  'Service Unavailable',
+  '所持金額が足りません',
+  'not_enough_free_balance'
+];
