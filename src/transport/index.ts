a5acb426e6d9b36a57a31692ea074b89bd7dbb35
bitrinjani <bit.rinjani@gmail.com>
2018-02-18 14:31:26 +0900

    refactor logging

--- src/transport/index.ts
+++ src/transport/index.ts
@@ -20,9 +20,6 @@ try {
   configRoot = getConfigRoot();
 } catch (ex) {
   console.log(ex.message);
-  // tslint:disable-next-line:max-line-length
-  console.log(`[ERROR] config.json was not found. The location of config.json has been changed from ./src to ./ since v2.3.0.
-[ERROR] 設定ファイルconfig.jsonが見つかりませんでした。v2.3.0から設定ファイルの場所が./src/config.jsonから./config.jsonに変更されました。`);
 }
 
 // console output
