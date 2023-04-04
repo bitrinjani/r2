265aa5f57358c81c2fec3a474ea9a7b04bd76044
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-04 19:39:30 +0900

    Delete tools directory

--- tools/clearPairs.ts
+++
@@ -1,9 +0,0 @@
-import { getActivePairStore } from '../src/ActivePairLevelStore';
-import { getChronoDB } from '../src/chrono';
-
-(async () => {
-  const store = getActivePairStore(getChronoDB());
-  console.log('Removing active pair cache...');
-  await store.delAll();
-  console.log('Done');
-})();
