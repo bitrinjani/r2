83dc6c3a3e5d68cc37ff1bcdaa17406b1452f858
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-05 11:40:01 +0900

    Delete dist directory

--- dist/Bitflyer/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.create = void 0;
-const BrokerAdapterImpl_1 = require("./BrokerAdapterImpl");
-function create(config) {
-    return new BrokerAdapterImpl_1.default(config);
-}
-exports.create = create;
