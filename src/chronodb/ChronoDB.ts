39394c468b085ebeb33b6b09603f688bf5faff0d
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-04-05 16:12:44 +0900

    fix some errors

--- src/chronodb/ChronoDB.ts
+++ src/chronodb/ChronoDB.ts
@@ -2,13 +2,28 @@ import { Level } from 'level';
 import mkdirp from 'mkdirp';
 import { LevelUp } from "./types";
 import { TimeSeries } from './TimeSeries';
+import { EntryStream, KeyStream } from 'level-read-stream';
+import { Readable } from 'stream';
 
 export class ChronoDB {
   private readonly store: LevelUp;
 
   constructor(path: string) {
     mkdirp.sync(path);
-    this.store = new Level(path, { keyEncoding: 'utf-8', valueEncoding: 'json' });
+    const store = new Level(path, { keyEncoding: 'utf-8', valueEncoding: 'json' });
+    this.store = {
+      put: store.put.bind(store),
+      del: store.del.bind(store),
+      get: store.get.bind(store),
+      isClosed: () => store.status === "closing" || store.status === "closed",
+      close: store.close.bind(store),
+      createReadStream: (options) => {
+        return new EntryStream(options) as unknown as Readable;
+      },
+      createKeyStream: (options) => {
+        return new KeyStream(options) as unknown as Readable;
+      },
+    };
   }
 
   getTimeSeries<T>(name: string, reviver?: (o: T) => T) {
