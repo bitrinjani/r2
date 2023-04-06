93c6adee412d6c20656d682a923993e213f0bb64
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-06 12:51:19 +0900

    ignore .env and update package.json

--- src/Coincheck/index.ts
+++ src/Coincheck/index.ts
@@ -1,6 +1,6 @@
 import BrokerAdapterImpl from './BrokerAdapterImpl';
 import { BrokerConfigType } from '../types';
 
-export function create(config: BrokerConfigType): BrokerAdapterImpl {
+export function create(config: BrokerConfigType) {
   return new BrokerAdapterImpl(config);
 }
