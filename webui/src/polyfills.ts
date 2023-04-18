74531fa1646ac00511a9a4254d05dd5774ec0c4f
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-18 16:56:54 +0900

    up

--- webui/src/polyfills.ts
+++ webui/src/polyfills.ts
@@ -1,11 +1,4 @@
-
-
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
 
 
 /***************************************************************************************************
